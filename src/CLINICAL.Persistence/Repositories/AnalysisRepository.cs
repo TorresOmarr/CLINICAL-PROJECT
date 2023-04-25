f1328228d42043a3c5c27a07d5dcef83a7ff7a4d
Omar Torres <jtorres@axsistec.com>
2023-04-25 15:44:31 -0600

    repository implemented

---
+++ src/CLINICAL.Persistence/Repositories/AnalysisRepository.cs
@@ -0,0 +1,34 @@
+﻿using CLINICAL.Application.Interface;
+using CLINICAL.Domain.Entitites;
+using CLINICAL.Persistence.Context;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLINICAL.Persistence.Repositories
+{
+    public class AnalysisRepository: IAnalysisRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public AnalysisRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Analysis>> ListAnalysis()
+        {
+            using var connection = _context.CreateConnection;
+
+            var query = "uspAnalysisList";
+
+            var analisys = await connection.QueryAsync<Analysis>(query, commandType: CommandType.StoredProcedure);
+
+            return analisys;
+
+        }
+    }
+}
