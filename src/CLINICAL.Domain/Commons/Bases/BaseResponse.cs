7fb7af6288b46850c5cf18de863da153d6dbc97c
Omar Torres <jtorres@axsistec.com>
2023-04-25 15:40:14 -0600

    interface analysis created

--- src/CLINICAL.Domain/Commons/Bases/BaseResponse.cs
+++ src/CLINICAL.Domain/Commons/Bases/BaseResponse.cs
@@ -5,6 +5,6 @@
         public bool isSucces { get; set; }
         public T? Data { get; set; }
         public string? Message { get; set; }
-        public IEnumerable<BaseError> Errors { get; set; }
+        public IEnumerable<BaseError>? Errors { get; set; }
     }
 }
