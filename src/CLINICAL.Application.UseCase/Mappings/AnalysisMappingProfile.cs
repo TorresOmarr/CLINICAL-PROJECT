0a862afdcb3cf851cb607a22461758d4e70590e6
Omar Torres <jtorres@axsistec.com>
2023-04-25 17:08:18 -0600

    Controller and Mapping Profile

---
+++ src/CLINICAL.Application.UseCase/Mappings/AnalysisMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CLINICAL.Application.Dtos.Analysis.Response;
+using CLINICAL.Domain.Entitites;
+
+namespace CLINICAL.Application.UseCase.Mappings
+{
+    public class AnalysisMappingProfile : Profile
+    {
+        public AnalysisMappingProfile()
+        {
+            CreateMap<Analysis, GetAllAnalysisResponseDto>()
+                .ForMember(x => x.StateAnalysis, x => x.MapFrom(x => x.State == 1 ? "Activo" : "Inactivo"));
+        }
+    }
+}
