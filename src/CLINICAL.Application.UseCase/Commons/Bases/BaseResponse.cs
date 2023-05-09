00e3bb8149e524324b0ea18d2f4b7334811f3ad5
Omar Torres <jtorres@axsistec.com>
2023-05-09 21:56:08 +0000

    added launch settings for vscode

--- src/CLINICAL.Application.UseCase/Commons/Bases/BaseResponse.cs
+++ src/CLINICAL.Application.UseCase/Commons/Bases/BaseResponse.cs
@@ -5,6 +5,10 @@
         public bool IsSucces { get; set; }
         public T? Data { get; set; }
         public string? Message { get; set; }
+<<<<<<< Updated upstream:src/CLINICAL.Application.UseCase/Commons/Bases/BaseResponse.cs
         public IEnumerable<BaseError>? Errors { get; set; }
+=======
+        public IEnumerable<BaseError>? Errors { get; set; } 
+>>>>>>> Stashed changes:src/CLINICAL.Domain/Commons/Bases/BaseResponse.cs
     }
 }
