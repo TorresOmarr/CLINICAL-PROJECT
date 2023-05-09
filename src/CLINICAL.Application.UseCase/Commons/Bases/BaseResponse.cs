26184d4ff517a11d3f1e7864eb5eeebfe861e847
Omar Torres <jtorres@axsistec.com>
2023-05-09 22:20:06 +0000

    devcontainerjson

--- src/CLINICAL.Application.UseCase/Commons/Bases/BaseResponse.cs
+++ src/CLINICAL.Application.UseCase/Commons/Bases/BaseResponse.cs
@@ -5,10 +5,6 @@
         public bool IsSucces { get; set; }
         public T? Data { get; set; }
         public string? Message { get; set; }
-<<<<<<< Updated upstream:src/CLINICAL.Application.UseCase/Commons/Bases/BaseResponse.cs
         public IEnumerable<BaseError>? Errors { get; set; }
-=======
-        public IEnumerable<BaseError>? Errors { get; set; } 
->>>>>>> Stashed changes:src/CLINICAL.Domain/Commons/Bases/BaseResponse.cs
     }
 }
